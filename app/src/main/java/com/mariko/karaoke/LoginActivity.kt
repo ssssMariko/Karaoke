cc099041b5f144f2c26f166f6bee9c7190d82b7a
韩进 <18502702013@163.com>
2022-09-07 12:12:27 +0800

    登录逻辑

--- app/src/main/java/com/mariko/karaoke/LoginActivity.kt
+++ app/src/main/java/com/mariko/karaoke/LoginActivity.kt
@@ -1,11 +1,59 @@
 package com.mariko.karaoke
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.mariko.karaoke.databinding.ActivityLoginBinding
+import com.tencent.liteav.basic.AvatarConstant
+import com.tencent.liteav.basic.UserModel
+import com.tencent.liteav.basic.UserModelManager
+import com.tencent.liteav.debug.GenerateTestUserSig
+import java.util.*
+
+private const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var mEditUserId: EditText
+    private lateinit var mButtonLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mEditUserId = binding.etUserId
+        mButtonLogin = binding.tvLogin
+        mButtonLogin.apply {
+            setOnClickListener {
+                login()
+            }
+        }
     }
+
+    private fun login() {
+        val userId = mEditUserId.text.toString().trim()
+        val userModel = UserModel()
+        userModel.apply {
+            this.userId = userId
+            userName = userId
+            userSig = GenerateTestUserSig.genTestUserSig(userId)
+            val index = Random().nextInt(AvatarConstant.USER_AVATAR_ARRAY.count())
+            val coverUrl = AvatarConstant.USER_AVATAR_ARRAY[index]
+            userAvatar = coverUrl
+        }
+        val manager = UserModelManager.getInstance()
+        manager.userModel = userModel
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
